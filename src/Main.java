b47a11f0b1eb783ed035e6358445372fdddea765
zizou <choizidane@naver.com>
2018-06-15 17:01:50 +0900

    메인.자바 꼬물꼬물

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		JFrame obj = new JFrame();
 		Gameplay gameplay = new Gameplay();
-		obj.setTitle("지렁이가 꿈틀꿈틀");
+		obj.setTitle("지렁이가 꼬물꼬물");
 		obj.setBounds(10,10,905,700);
 		obj.setBackground(Color.DARK_GRAY);
 		obj.setResizable(false);
